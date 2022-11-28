e13781dd2be885f74a0aa8789ee4db91d4302135
csotiistvan <109945490+csotiistvan@users.noreply.github.com>
2022-11-29 00:05:37 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,13 +6,6 @@ resource "aws_vpc" "schiele" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.schiele.id
-   tags = {
-    Name = "Schiele internet gateway"
-  }
-}
-
 resource "aws_subnet" "sch-public1" {
   vpc_id     = aws_vpc.schiele.id
   cidr_block = "10.0.1.0/24"
@@ -56,4 +49,45 @@ resource "aws_db_subnet_group" "subnet_group" {
   tags = {
     Name = "My DB subnet group"
   }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.schiele.id
+   tags = {
+    Name = "Schiele internet gateway"
+  }
+}
+
+resource "aws_nat_gateway" "sch_nat" {
+  allocation_id = aws_eip.sch_nat.id
+  subnet_id     = aws_subnet.sch-public1.id
+
+  tags = {
+    Name = "NAT Gateway"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table" "sch_route_table" {
+  vpc_id = aws_vpc.schiele.id
+
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+}
+
+resource "aws_route_table_association" "rta_subnet_public1" {
+  subnet_id      = aws_subnet.sch-public1.id
+  route_table_id = aws_route_table.sch_route_table.id
+}
+
+resource "aws_route_table_association" "rta_subnet_public2" {
+  subnet_id      = aws_subnet.sch-public2.id
+  route_table_id = aws_route_table.sch_route_table.id
 }
